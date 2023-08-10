2b09baeb1ec8ce6e8b9ba4bbc2c2b4cc320c9f82
Soumen Jana <soumyaroy13336@gmail.com>
2023-08-10 21:07:16 +0530

    update

---
+++ js.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    const student = ["Soumen", "Suvraneel", "Parag", "Debasish"];
+    document.getElementById("demo").innerHTML = student[0];
+}
+function myFunction1() {
+    const student = ["Soumen", "Suvraneel", "Parag", "Debasish"];
+    document.getElementById("demo").innerHTML = student[1];
+}
+function myFunction2() {
+    const student = ["Soumen", "Suvraneel", "Parag", "Debasish"];
+    document.getElementById("demo").innerHTML = student[2];
+}
+function myFunction3() {
+    const student = ["Soumen", "Suvraneel", "Parag", "Debasish"];
+    document.getElementById("demo").innerHTML = student[3];
+}
