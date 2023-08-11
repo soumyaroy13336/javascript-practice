279236ea6df27d1d4b356d2c3fa5f3418b771e27
Soumen Jana <soumyaroy13336@gmail.com>
2023-08-11 12:42:22 +0530

    update practice set 6

--- practice-set-5/practice-set-6/set-6.js
+++ practice-set-5/practice-set-6/set-6.js
@@ -8,5 +8,20 @@ arr1.push(result1)
 // 2. Keep adding numbers to the array in 1 until 0 is added to the array
 
 // 3. Filter for numbers divisible by 10 from a given array
-
+function findSmallest(p, n)
+{
+    for (let i = 0; i < n; i++)
+    {
+        let j;
+        for (j = 0; j < n; j++)
+            if (p[j] % p[i]>=1)
+                break;
+        if (j == n)
+            return p[i];
+    }       
+    return -1;
+}
+    let p = [ 25, 20, 5, 10, 100 ];
+    let n = g.length;
+    console.log(findSmallest(p, n));
 // 4. Create an array for square of given numbers
